b7c13a4c46bc1b7efddba2ed96a65d00c3f8f1a8
Duver Insignares <duverarleyinsignares@gmail.com>
2021-09-17 08:59:15 -0500

    Se agrega el Navbar

---
+++ buttons.php
@@ -0,0 +1,10 @@
+<?php 
+include_once "includes/header.php";?>
+    <!-- // Contenido Principal -->
+    <div class="container">
+        <h1 class="text-center">Página de Buttons</h1>
+    </div>
+    <!-- // Final contenido Principal -->
+<?php 
+    include_once "includes/footer.php";?>
+    
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once "includes/header.php"; ?>
+<!-- // Contenido Principal -->
+<div class="container">
+    <h1 class="text-center">Contenido Principal</h1>
+</div>
+<!-- // Final contenido Principal -->
+<?php include_once "includes/footer.php"; ?>
